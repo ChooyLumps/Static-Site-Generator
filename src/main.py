11d9d2d1f8440edbfbe92f044defb3fcc0c348ee
chris_p_mato <chris.y.patillo@gmail.com>
2025-11-17 21:07:01 -0500

    TextNode Tested

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
-print("hello world")
+from textnode import TextNode, TextType
+
+def main():
+    # Example usage of TextNode
+    plain_text = TextNode("Hello, World!")
+    bold_text = TextNode("This is bold text", TextType.BOLD)
+    link_text = TextNode("Click here", TextType.LINK, url="https://example.com")
+    
+    print(plain_text)
+    print(bold_text)
+    print(link_text)
+
+main()
