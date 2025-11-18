5ea258399efa07cf2499132eb183687daec31363
chris_p_mato <chris.y.patillo@gmail.com>
2025-11-18 13:35:37 -0500

    LeafNode Unit Tested

---
+++ src/test_leafnode.py
@@ -0,0 +1,22 @@
+import unittest
+
+from leafnode import LeafNode
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_props(self):
+        node = LeafNode("a", "Click here", props={"href": "https://example.com"})
+        self.assertEqual(node.to_html(), '<a href="https://example.com">Click here</a>')
+    
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Just some text")
+        self.assertEqual(node.to_html(), "Just some text")
+
+    def test_leaf_to_html_no_value(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
