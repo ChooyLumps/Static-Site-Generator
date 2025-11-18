a4d1167d92fea069a8ab790a774237dc1439c5ca
chris_p_mato <chris.y.patillo@gmail.com>
2025-11-17 21:24:11 -0500

    TextNode Tested 2

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -8,6 +8,26 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
+    
+    def test_neq_different_text(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is another text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_neq_different_type(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+    
+    def test_neq_different_url(self):
+        node = TextNode("Click here", TextType.LINK, url="https://example.com")
+        node2 = TextNode("Click here", TextType.LINK, url="https://different.com")
+        self.assertNotEqual(node, node2)
+
+    def test_neq_different_url_None(self):
+        node = TextNode("Click here", TextType.LINK, url="https://example.com")
+        node2 = TextNode("Click here", TextType.LINK)
+        self.assertNotEqual(node, node2)
 
 
 if __name__ == "__main__":
