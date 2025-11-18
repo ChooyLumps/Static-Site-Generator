596316289deb4be0cfe64fc5a90b0daa83643f28
chris_p_mato <chris.y.patillo@gmail.com>
2025-11-17 21:18:06 -0500

    TextNode Unit Tested

--- src/textnode.py
+++ src/textnode.py
@@ -14,12 +14,12 @@ class TextNode:
         self.text_type = text_type
         self.url = url
     
-    def __eg__(self, other):
+    def __eq__(self, other):
         if not isinstance(other, TextNode):
             return False
-        return (self.text == other.text and 
-                self.text_type == other.text_type and 
-                self.url == other.url)
+        if self.text == other.text and self.text_type == other.text_type and self.url == other.url:
+            return True
+        return False
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
