e78098537173962b3d64cbe9dffb19b93dea1a30
chris_p_mato <chris.y.patillo@gmail.com>
2025-11-24 15:39:53 -0500

    Generate Webpage Implimented

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
-from textnode import TextNode, TextType
+from generators import generate_page
 from copier import purge_and_copy_from_static_to_public
 
 def main():
     purge_and_copy_from_static_to_public()
+    generate_page("content/index.md", "template.html", "public/index.html")
+
 
 main()
