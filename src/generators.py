6f4dd21a4fed5cb052b0bd49ada44701e5a601fc
chris_p_mato <chris.y.patillo@gmail.com>
2025-11-24 19:47:06 -0500

    Bug Fixes

--- src/generators.py
+++ src/generators.py
@@ -4,7 +4,7 @@ from MD_to_HTML import md_to_html_nodes
 from extractors import extract_markdown_title
 
 
-def generate_page(markdown_path: str, template_path: str, output_path: str, basepath: str = "/"):
+def generate_page(markdown_path: str, template_path: str, output_path: str, basepath: str):
     print(f"Generating page from {markdown_path} using template {template_path} to {output_path}")
     markdown = open(markdown_path).read()
     template = open(template_path).read()
@@ -24,4 +24,4 @@ def generate_page_recursive(content_path: str, template_path: str, output_dir: s
                 relative_path = os.path.relpath(root, content_path)
                 output_path = os.path.join(output_dir, relative_path, file.replace(".md", ".html"))
                 markdown_path = os.path.join(root, file)
-                generate_page(markdown_path, template_path, output_path)
+                generate_page(markdown_path, template_path, output_path, basepath)
--- src/main.py
+++ src/main.py
@@ -4,13 +4,14 @@ from generators import generate_page_recursive
 from copier import purge_and_copy
 
 def main():
-    if len(sys.argv) > 1:
-        basepath = sys.argv[1]
+    args = sys.argv
+    if len(args) > 1:
+        basepath = args[1]
     else:
         basepath = "/"
     target = "docs"
     purge_and_copy("static", f"{target}")
-    generate_page_recursive("content", "template.html", f"{target}", basepath)
+    generate_page_recursive("content", "template.html", f"{target}", f"{basepath}")
 
 
 main()
